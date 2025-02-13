3af1662d0bec596a176b217de64db37e78b3a91f
PremSagar <premsagars.1811@gmail.com>
2025-02-14 01:15:14 +0530

    course repo

--- src/main/java/com/example/my_lms_app/model/Course.java
+++ src/main/java/com/example/my_lms_app/model/Course.java
@@ -1,11 +1,30 @@
 package com.example.my_lms_app.model;
 
-import java.util.List;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.List;
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name= "course")
 public class Course {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(nullable = false)
     private String title;
+    @Column()
     private String description;
+
+    @Column(nullable = false)
     private User instructor;
+
+    @ElementCollection
     private List<User> students;
 }
--- src/main/java/com/example/my_lms_app/repository/CourseRepository.java
+++ src/main/java/com/example/my_lms_app/repository/CourseRepository.java
@@ -1,4 +1,14 @@
 package com.example.my_lms_app.repository;
 
-public interface CourseRepository {
+import com.example.my_lms_app.model.Course;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CourseRepository extends JpaRepository<Course, Long> {
+
+    public String findCourseById(Long id);
+
+    public String findCourseByTitle(String title);
+
 }
